9976d1196d2a9931e915d89317b57f14004f6c0d
quangtnv182422 <quangtnvhe182422@fpt.edu.vn>
2024-05-11 16:42:01 +0700

    Update file template

---
+++ ProjectSWP/src/java/dal/DBContext.java
@@ -0,0 +1,28 @@
+
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP_Query";
+            String username = "sa";
+            String password = "123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+     public static void main (String args[]){
+        DBContext dao = new DBContext();
+        System.out.println(dao);
+    }
+    
+}
