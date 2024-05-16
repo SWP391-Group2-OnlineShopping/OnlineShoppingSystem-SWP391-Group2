7a6d371065c5ed2cbf80ad22f9561c1a897299e2
nguyentunglamhe180410 <lamnthe180410@fpt.edu.vn>
2024-05-16 12:25:06 +0700

    create images model

---
+++ ProjectSWP/src/java/model/Images.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author DELL
+ */
+public class Images {
+    private int imageID;
+    private String link;
+
+    
+    //default constructor
+    public Images() {
+    }
+    //constructor with params
+    public Images(int imageID, String link) {
+        this.imageID = imageID;
+        this.link = link;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public String toString() {
+        return "Images{" + "imageID=" + imageID + ", link=" + link + '}';
+    }
+    
+}
