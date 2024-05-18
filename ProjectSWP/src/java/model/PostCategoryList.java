9a8e56da832daca8c1c8785ab248c2f3b7a4cf3e
nguyentunglamhe180410 <lamnthe180410@fpt.edu.vn>
2024-05-18 19:31:02 +0700

    add Post Categories List

---
+++ ProjectSWP/src/java/model/PostCategoryList.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author DELL
+ */
+public class PostCategoryList {
+    private int PostCL;
+    private String Name;
+    private String Description;
+    
+    //Default constructor
+    public PostCategoryList() {
+    }
+    //constructor with params
+    public PostCategoryList(int PostCL, String Name, String Description) {
+        this.PostCL = PostCL;
+        this.Name = Name;
+        this.Description = Description;
+    }
+
+    public int getPostCL() {
+        return PostCL;
+    }
+
+    public void setPostCL(int PostCL) {
+        this.PostCL = PostCL;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    @Override
+    public String toString() {
+        return "PostCategoryList{" + "PostCL=" + PostCL + ", Name=" + Name + ", Description=" + Description + '}';
+    }
+    
+}
