22a1a829cc9c2069a81375d794add09a29c2632c
nguyentunglamhe180410 <lamnthe180410@fpt.edu.vn>
2024-05-27 14:19:30 +0700

    modify feature blog and blog.jsp

--- ProjectSWP/src/java/dal/DBContext.java
+++ ProjectSWP/src/java/dal/DBContext.java
@@ -10,9 +10,9 @@ public class DBContext {
     public DBContext()
     {
         try {
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP_Query;trustServerCertificate=true";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP_Query_Group6;trustServerCertificate=true";
             String username = "sa";
-            String password = "123";
+            String password = "sa";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
